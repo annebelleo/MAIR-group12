c3d7e6684492ca367a8ffae0fe6b7c851e2185b4
TomasIken <tomas.iken@gmail.com>
2023-09-18 17:13:05 +0200

    preference extraction update

--- src/preference_extraction.py
+++ src/preference_extraction.py
@@ -1,9 +1,10 @@
 import Levenshtein as lev
 import pandas as pd
+import itertools
 def load_restaurant_data(file_path = 'res/restaurant_info.csv'):
     df = pd.read_csv(file_path)
     # there are some NaN comming in area
-    
+    # issue multiple word
     result = {}
     result["pricerange"] = df["pricerange"].drop_duplicates()
     result["area"] = df["area"].drop_duplicates()
@@ -14,19 +15,26 @@ def load_restaurant_data(file_path = 'res/restaurant_info.csv'):
     result["food"] = result["food"].dropna(axis = 0)
     
     return result
-
+def get_combinations(sentence):
+    sentence = sentence.split()
+    xs = [sentence[i:j] for i, j in itertools.combinations(range(len(sentence)+1), 2)]
+    output = []
+    for i in xs:
+        output.append(" ".join(i))
+    return output
 def get_preference(sentence):
     restaurant_data = load_restaurant_data()
+    xs = get_combinations(sentence)
     result = {}
-    for word in sentence.split():
+    for word in xs:
         for key in restaurant_data.keys():
             for item in restaurant_data[key]:
-                if lev.distance(word, item) < 3:
-                    print(word, item)
+                max_distance = len(word)//3
+                if lev.distance(word, item) < max_distance:
                     if key not in result:
                         result[key] = []
                         result[key].append(item)
         
     return result
 
-print(get_preference("i want thai food"))
+print(get_preference("i want acheap azian orientil food in the center of town"))
