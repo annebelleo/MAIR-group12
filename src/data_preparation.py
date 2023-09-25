5bfc64c31b1de00f6f3471ef565add6783d8b759
Merxra <lx.matthijs@gmail.com>
2023-09-25 13:54:36 +0200

    CSV_add_qualities() to data_preparation for 1c

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,5 +1,6 @@
 # By MatthijsvL
 import pandas as pd
+from random import randint
 
 ### deprecated: included in main
 descriptions = ['ack','affirm','bye','confirm','deny','hello',
@@ -28,5 +29,35 @@ def get_data(path_dialog_acts = 'res/dialog_acts.dat', drop_duplicates = False):
 
   return dialogue_df
 
+def CSV_add_qualities(path_origin, path_destination):
+  
+  assert path_origin != path_destination, 'lets not overwrite old file'
+  
+  DF_restaurants = pd.read_csv(path_origin)
+  
+  nrows = DF_restaurants.shape[0]
+  
+  vals_food_quality = ['good', 'mid', 'bad', 'trash']
+  vals_crowdedness = ['crowded', 'quiet']
+  vals_length_of_stay = ['short', 'moderate' ,'long']
+  
+  DF_restaurants['food_quality'] = DF_restaurants.apply(
+    lambda x: vals_food_quality[randint(0, len(vals_food_quality)-1)],
+    axis = 1
+    )
+  
+  DF_restaurants['crowdedness'] = DF_restaurants.apply(
+    lambda x: vals_crowdedness[randint(0, len(vals_crowdedness)-1)],
+    axis = 1
+    )
+  
+  DF_restaurants['length_of_stay'] = DF_restaurants.apply(
+    lambda x: vals_length_of_stay[randint(0, len(vals_length_of_stay)-1)],
+    axis = 1
+    )
+  DF_restaurants.to_csv(path_destination)
+  
+if __name__ == '__main__':
+  CSV_add_qualities(path_origin='res/restaurant_info.csv', path_destination='res/restaurant_extra_info.csv')
 
 
