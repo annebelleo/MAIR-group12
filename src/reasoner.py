e546495a1c28b43550c2c41ff092e7757b682f30
TomasIken <tomas.iken@gmail.com>
2023-10-03 13:25:09 +0200

    added comments to reasoning file

--- src/reasoner.py
+++ src/reasoner.py
@@ -5,36 +5,36 @@ def camel_case(s):
   s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
   return ''.join([s[0].lower(), s[1:]])
 
-class Reasoner:
+class Reasoner: # Main reasoner class. Representation of KB is stored here
     def __init__(self):
         self.init_knowledge_base()
-    def init_knowledge_base(self):
+    def init_knowledge_base(self): # Initialization of rules specified in task 1c
         self.knowledge_base = pl.KnowledgeBase("restaurants")
-        self.knowledge_base(["touristic(X) :- restaurant(X,good,_,_,_,cheap)",
+        self.knowledge_base(["touristic(X) :- restaurant(X,good,_,_,_,cheap)", #Ex. X is touristic if it has good food, is cheap and all other values are irrelavant
                 "notTouristic(X) :- restaurant(X,_,_,_,romanian,_)",
                 "assignedSeats(X) :- restaurant(X,_,busy,_,_,_)",
                 "notRomantic(X) :- restaurant(X,_,busy,_,_,_)",
                 "notChildren(X) :- restaurant(X,_,_,long,_,_)",
                 "romantic(X) :- restaurant(X,_,_,long,_,_)",
-                "neq(touristic(X),notTouristic(X))",
+                "neq(touristic(X),notTouristic(X))", # Negation. X cannot be touristic and not touristic
                 "neq(notRomantic(X),romantic(X))"])
-    def query(self,query,find_all = False):
-        if find_all:
+    def query(self,query,find_all = False): # Query function
+        if find_all: # If wants to query with variables
             return self.knowledge_base.query(pl.Expr(query),show_path=True)
-        else:
+        else: # if wants a true false query. In this case all uppercase first characters are turned to camelcase to represent constants
             res = findall(r'\(.*?\)', query)
             res = res[0][1:-1]
             query =query.replace(res,camel_case(res))
             return self.knowledge_base.query(pl.Expr(query),show_path=True)
         
-    def add_facts(self,df):
+    def add_facts(self,df): # Function to add new fact to the kb
         for i in range(df.shape[0]):
             # restaurant(Name,quality,crowd,length,food,price)
             x = f"restaurant({camel_case(df.restaurantname[i])},{camel_case(df.food_quality[i])},{camel_case(df.crowdedness[i])},{camel_case(df.length_of_stay[i])},{camel_case(df.food[i])},{camel_case(df.pricerange[i])})"
             self.knowledge_base([x])
 
 
-def filter(prefiltered_list,filters):
+def filter(prefiltered_list,filters): # Get all restaurants that satisfy a rule as specified in filter
     output= []
     
     reasoner = Reasoner()
@@ -42,7 +42,6 @@ def filter(prefiltered_list,filters):
     for x in range(prefiltered_list.shape[0]):
         q=reasoner.query(f"{filters}({prefiltered_list.iloc[x].restaurantname}))")
         if q[0]==["Yes"]:
-           # output = pd.concat([output, prefiltered_list.iloc[x]], join='inner')
             output.append(prefiltered_list.iloc[x])
     return output
 
@@ -53,9 +52,11 @@ def get_nth_key(dictionary, n=0):
         if i == n:
             return key
     raise IndexError("dictionary index out of range")
+
 def un_camel_caseify(string):
     return sub(r'((?<=[a-z])[A-Z]|(?<!\A)[A-Z](?=[a-z]))', r' \1', string).lower()
-def get_reasoning(q):
+
+def get_reasoning(q): # String manipulation to turn the rule applied to a natural language sentence
     rule_format = ["name","quality","crowd","lengthOfStay","foodType","price"]
     reasoner= Reasoner()
     rule = str(reasoner.knowledge_base.db[q]["facts"][0])
